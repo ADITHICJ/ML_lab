08e9a7a2e42c074ddb1d37edeb46e0d4dc1ce56a
Adithi C J <163150837+ADITHICJ@users.noreply.github.com>
2026-07-27 17:29:19 +0530

    Update a.py

--- 7/a.py
+++ 7/a.py
@@ -2,6 +2,7 @@ from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
+import numpy as np
 
 glass = fetch_openml("glass", as_frame=True)
 
@@ -12,18 +13,34 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
 
-for metric in ['euclidean', 'manhattan']:
-    knn = KNeighborsClassifier(n_neighbors=3, metric=metric)
+# Euclidean Distance Formula
+def euclidean_distance(x, y):
+    return np.sqrt(np.sum((x - y) ** 2))
+
+# Manhattan Distance Formula
+def manhattan_distance(x, y):
+    return np.sum(np.abs(x - y))
+
+metrics = {
+    "Euclidean": euclidean_distance,
+    "Manhattan": manhattan_distance
+}
+
+for name, distance_function in metrics.items():
+
+    knn = KNeighborsClassifier(
+        n_neighbors=3,
+        metric=distance_function
+    )
 
     knn.fit(X_train, y_train)
 
     y_pred = knn.predict(X_test)
 
-    print(f"\nKNN using {metric.capitalize()} Distance")
+    print(f"\nKNN using {name} Distance")
     print(f"Accuracy: {accuracy_score(y_test, y_pred) * 100:.2f}%")
     print("Confusion Matrix:")
     print(confusion_matrix(y_test, y_pred))
-
 
 # KNN using Euclidean Distance
 # Accuracy: 69.23%
